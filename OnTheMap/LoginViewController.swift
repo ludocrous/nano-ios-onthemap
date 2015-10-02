cc0c04575f34d3152336947fa9677b9578cd29cd
Derek Crous <ludocrousdev@gmail.com>
2015-10-02 17:41:44 +0100

    Complete rudimentary login view for Udacity login

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Derek Crous on 02/10/2015.
+//  Copyright © 2015 Ludocrous Software. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+   
+    
+    @IBAction func signUpButtonTouch(sender: UIButton) {
+    }
+    
+    @IBAction func loginButtonTouch(sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
